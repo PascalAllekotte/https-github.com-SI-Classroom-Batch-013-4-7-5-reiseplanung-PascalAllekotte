a1bc4f10b829998600a12ef302fcf63c9730fbe3
PascalAllekotte <pascalallekotte1@web.de>
2024-06-14 20:00:45 +0200

    mf

---
+++ TravelPlaner/TabViews/GlobusView/Detail/GlobusDetail.swift
@@ -0,0 +1,18 @@
+//
+//  GlobusDetail.swift
+//  TravelPlaner
+//
+//  Created by Pascal Allekotte on 14.06.24.
+//
+
+import SwiftUI
+
+struct GlobusDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlobusDetail()
+}
