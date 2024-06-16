5642964a70c2a585821483b7728df58c5d6d22d3
PascalAllekotte <pascalallekotte1@web.de>
2024-06-17 00:58:11 +0200

    klappt

---
+++ TravelPlaner/TabViews/GlobusView/SightView/SightDetail.swift
@@ -0,0 +1,18 @@
+//
+//  SightDetail.swift
+//  TravelPlaner
+//
+//  Created by Pascal Allekotte on 16.06.24.
+//
+
+import SwiftUI
+
+struct SightDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SightDetail()
+}
---
+++ TravelPlaner/TabViews/GlobusView/SightView/addSight.swift
@@ -0,0 +1,18 @@
+//
+//  addSight.swift
+//  TravelPlaner
+//
+//  Created by Pascal Allekotte on 16.06.24.
+//
+
+import SwiftUI
+
+struct addSight: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addSight()
+}
